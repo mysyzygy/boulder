2f9c92a0a6a208123c8ac02c7a41226c849b4e86
Derek Jones <mysyzygy@gmail.com>
2024-06-17 00:36:41 -0700

    add config

--- app/__init__.py
+++ app/__init__.py
@@ -5,22 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import migrate, Migrate
 from datetime import datetime
 import os
+from config import Config
 
 from datetime import datetime
 
 db = SQLAlchemy()
+migrate = Migrate()
 
-def create_app(test_config=None):
+
+def create_app(config_class=Config):
     # create and configure the app
     app = Flask(__name__, template_folder='templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
+    app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import register_routes
     register_routes(app, db)
-
-    migrate = Migrate(app, db)
 
     return app
 
