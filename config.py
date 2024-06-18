27f0db80d00a337d078041d04076b11a07cae2a8
Derek Jones <mysyzygy@gmail.com>
2024-06-17 23:38:35 -0700

    update config and deps for postgreSQL

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1) if os.environ.get('DATABASE_URL') else 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
